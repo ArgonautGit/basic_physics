e8b18b2d57d203ab1c1af4d2efdf46bf2d039b78
Nicholas.Sara <nick-github@vansid.org>
2025-07-01 16:18:06 +0000

    Added basic egui test

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ use crate::motion::{Motion, MotionState};
 mod motion;
 pub mod units;
 
-fn main() {
+mod ui;
+
+fn main() -> eframe::Result {
     let mut motion = MotionState::default();
     motion.forces.push(Force::new(1.0, 1.0));
 
     for _ in 1..100 {
-        dbg!(motion.update_motion(1.0));
+        (motion.update_motion(1.0));
         // println!("Position: {} Velocity: {}", motion.position, motion.velocity);
     }
+
+    ui::run()
 }
---
+++ src/ui.rs
@@ -0,0 +1,34 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::{egui, Frame};
+use egui::{Button, Color32, Pos2};
+
+pub fn run() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size((300.0, 300.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native("Test App", options, Box::new(|_cc| Ok(Box::<MyApp>::default())))
+}
+
+#[derive(Default)]
+struct MyApp {
+    // Put state here.
+}
+
+impl eframe::App for MyApp {
+    // Runs every frame.
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+         egui::CentralPanel::default().show(ctx, |ui| {
+            
+
+            ui.heading("Physics Simulator");
+
+            let painter = ui.painter();
+            painter.circle_filled(Pos2::new(150.0, 150.0), 100.0, egui::Color32::DARK_BLUE);
+        });
+
+        ctx.request_repaint();
+    }
+}
